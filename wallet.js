d38c5d7540f359e3d0adf0f01c430951998f875c
Tether-creator <gruthzillions@gmail.com>
2025-03-21 22:24:11 +0100

    Delete wallet.js

--- wallet.js
+++
@@ -1,6 +0,0 @@
-// scripts/wallet.js
-
-document.querySelector('.connect-wallet').addEventListener('click', async () => {
-  alert("WalletConnect integration coming soon!");
-  // Placeholder for WalletConnect, Trust Wallet, MetaMask, etc.
-});
