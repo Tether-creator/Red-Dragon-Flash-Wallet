dbc40661d26916b924ed30cdb64485f9c80db2c3
Tether-creator <gruthzillions@gmail.com>
2025-03-21 22:18:05 +0100

    Create wallet.js

---
+++ wallet.js
@@ -0,0 +1,6 @@
+// scripts/wallet.js
+
+document.querySelector('.connect-wallet').addEventListener('click', async () => {
+  alert("WalletConnect integration coming soon!");
+  // Placeholder for WalletConnect, Trust Wallet, MetaMask, etc.
+});
